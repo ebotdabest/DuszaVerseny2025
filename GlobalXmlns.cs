9a484cb0a38a0e66790d493ab0fa963eafca0f13
EBot <66640213+ebotdabest@users.noreply.github.com>
2025-11-07 12:27:09 +0100

    FirstCommit Happy working

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "DuszaVerseny2025")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "DuszaVerseny2025.Pages")]
